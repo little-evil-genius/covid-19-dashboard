a72fe420e0e9a272298fa761c7d8d13af29464e7
alegsch <150163783+alegsch@users.noreply.github.com>
2023-11-21 10:16:24 +0100

    Add files via upload

---
+++ PackagesToInstall.R
@@ -0,0 +1,25 @@
+# Core packages
+install.packages("tidyverse") # How we work with data
+install.packages("shiny") # Interactive web apps, no HTML or Javascript required
+install.packages("rmarkdown") # Publish your work in a range of formats
+
+# Libraries that extend RMarkdown
+install.packages("flexdashboard") # Interactive Dashboards 
+install.packages("blogdown") # Blogs
+install.packages("bookdown") # Books
+install.packages("pkgdown") # Package documentation website
+install.packages("revealjs") # Slides
+
+# Useful analytics packages
+install.packages("tidyquant") # Financial markets and timeseries
+install.packages("data.table") # Work with large datasets fast
+install.packages("tidygraph") # Graph network analysis using the Tidyverse standards
+install.packages("igraph") # The original graph network package for R
+install.packages("lubridate") # Working with dates
+
+# Visualization Libraries
+install.packages("DT") # Interactive Tables for your apps
+install.packages("visNetwork") # Graph network Visualizations
+install.packages("plotly") # Interactive Visualizations
+install.packages("highcharter") # An alternative to Plotly
+
